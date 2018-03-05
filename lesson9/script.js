bbb400cc1bcfdd55ab92ae382f0fc9ff0371756c
Misha Yartsev <siliconchik@gmail.com>
2018-03-05 19:56:02 +0300

    + 2 first tasks

--- lesson9/script.js
+++ lesson9/script.js
@@ -2,4 +2,22 @@
  *
  */ 
 
- 
+
+// ## 1. 
+function argSum() {
+	let args = [].slice.call(arguments);
+	return args.reduce(function(a, b) {
+		return a + b;					
+	});
+}
+
+//  console.log(argSum(5, 25, 50, 33, 67))
+
+
+// ## 2. 
+function applyAll() {
+	let args = [].slice.call(arguments, 1)	
+	return arguments[0].apply(this, args);
+}
+
+//  console.log(applyAll(argSum, 25, 50, 33, 67))
